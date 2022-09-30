fa1369603e2d5373dfc471101025742586fe2986
Ankitthalwal <87529378+Ankitthalwal@users.noreply.github.com>
2022-09-30 09:41:04 +0530

    Some changes

---
+++ js.js
@@ -0,0 +1,13 @@
+document.getElementById("jj").addEventListener("click",abc);
+
+function abc(){
+  let hh = document.getElementById("mnu");
+if (hh.style.display=="block"){
+  hh.style.display="none";
+}
+else{
+  hh.style.display="block";
+}
+}
+
+
